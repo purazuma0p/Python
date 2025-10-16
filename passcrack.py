57786e945876b7befbddd8c3f177109b01a89da6
ぷらずま <zuxhxb@gmail.com>
2025-10-17 02:30:46 +0900

    Update passcrack.py

--- passcrack.py
+++ passcrack.py
@@ -4,16 +4,13 @@ import sys
 import time
 
 RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-RESET = '\033[0m'
 for password in open('password.txt'):
     password = password.strip()
     time.sleep(1)
     with open('password.txt') as f:
         for line in f:
             if password in line:
-                print(RED + f"[＊]192.168.3.1>31.45.123.78:> {password}" + RESET)
+                print(RED + f"[＊]found:> {password}" )
                 break
             time.sleep(0.01)
         # Simulate processing time
